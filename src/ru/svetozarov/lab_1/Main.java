fe4b1924b47e2577ba9b998bfcd5e0b10bab7c18
Evgenij Svetozarov <evgenij.svetozarov@yandex.ru>
2017-02-16 02:10:54 +0700

    v 1.1

--- src/ru/svetozarov/lab_1/Main.java
+++ src/ru/svetozarov/lab_1/Main.java
@@ -1,10 +1,7 @@
 package ru.svetozarov.lab_1;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
-import ru.svetozarov.lab_1.handler_file.ReaderFile;
-
-import java.io.IOException;
-import java.util.ArrayList;
+import ru.svetozarov.lab_1.thread_maker.ThreadMaker;
 
 
 /**
@@ -22,40 +19,14 @@ public class Main {
      * @param args массив ресурсов, с которых считываем данные
      */
     public static void main(String[] args) {
-        if (args.length == 0) {
-            logger.warn("Select resources....");
-        } else {
-            ArrayList<ReaderFile> readersArray=new ArrayList<>(args.length);
-            WordBook wordBook = new WordBook();
-            for (String arg :
-                    args) {
-                ReaderFile readerFile = new ReaderFile(arg, wordBook);
-                readersArray.add(readerFile);
-                readerFile.start();
-            }
-
-            for (ReaderFile readerFile :
-                    readersArray) {
-                try {
-                    readerFile.join();
-                } catch (InterruptedException e) {
-                    logger.error("Error interrupted..");
-                }
-            }
-            try {
-                wordBook.writeFile("result.txt");
-            } catch (IOException e) {
-                logger.error("Error writing file.");
-            }
-
-            if (!wordBook.flagError) {
-                logger.info("Process succesfully completed.");
-            } else {
-                logger.info("Process failed.");
-            }
-
-            logger.info("The result is stored in result.txt");
-
+        WordBook wordBook = new WordBook();
+        ThreadMaker threadMaker = new ThreadMaker(args, wordBook);
+        if(threadMaker.createReaderFile()){
+            threadMaker.startReading();
         }
+        else {
+            logger.warn("Select resources....");
+        }
+
     }
 }
